d2cbc5c59f2d4f8024ec6f69f71dbd45e788e2d3
Asrath <pasrath.2004@gmail.com>
2025-07-29 11:42:35 +0530

    added router in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,7 @@
-import { Button } from '@/components/ui/button';
+import AppRouter from './AppRouter';
+
 function App() {
-  return (
-    <div>
-      <Button>Button</Button>
-    </div>
-  );
+  return <AppRouter />;
 }
 
 export default App;
